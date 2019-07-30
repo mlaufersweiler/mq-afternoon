e86855950bae03fd14a8a7be3e26f969bae7a5c5
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook.local>
2019-07-30 13:55:48 -0500

    finsihed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import './reset.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      links: ["SERVICES", "PORTFOLIO", "ABOUT", "TEAM", "CONTACT"],
+      hiddenMenu: false
+    };
+  }
+  showMenu = () => {
+    this.setState({hiddenMenu: !this.state.hiddenMenu})
+  }
 
+  render(){
+    const linkMap = this.state.links.map((element, index) => {
+      return <li key={index}>{element}</li>
+    });
+    return (
+      <div className="App">
+        <div className="button-container">
+          <button className="hidden-button"
+          type="submit"
+          onClick={this.showMenu}>MENU</button>
+        </div>
+        <div className="background-img">
+          <nav className="nav-bar">
+            <h2 className="startBootstrap">Start Bootstrap</h2>
+            <ul className="menu">{linkMap}</ul>
+            {this.state.hiddenMenu && <ul className="hidden-menu">{linkMap}</ul> }
+          </nav>
+          <header>
+            <div className="container">
+              <div className="overlay">
+                <div className="intro">
+                  <p className="welcome">Welcome To Our Studio!</p>
+                  <p className="call">IT'S NICE TO MEET YOU!</p>
+                  <button className="call-button">TELL ME MORE</button>
+                </div>
+              </div>
+            </div>
+          </header>
+        </div>
+      </div>
+    )
+  }
+}
 export default App;
